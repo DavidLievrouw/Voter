ebf84e9feb97d0054a2e3393fd5fd05004cf14d8
David Lievrouw <dlievrouw2@csc.com>
2017-02-21 10:30:37 +0100

    Add initial load script

---
+++ src/Database/InitialLoad.sql
@@ -0,0 +1,11 @@
+﻿INSERT INTO [security].[User] ([Login], [Password], [Salt], [FirstName], [LastName]) VALUES (
+  'dali',
+  'TBA',
+  'TBA',
+  'David',
+  'Lievrouw'
+);
+GO
+
+INSERT INTO [dbo].[version] ([Number], [Creation]) VALUES (1, GETDATE());
+GO
